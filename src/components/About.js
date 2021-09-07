057132ac3ae71d82b371133d185fa9abc30a365e
London <london.rosemary@gmail.com>
2021-09-07 18:30:40 -0400

    commit tree a4b6e88594b17f945e2942fac340df509e86b8a9

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  let bio;
+  if (props.bio) {
+    bio = <p>{props.bio}</p> 
+  }
+  else {
+    bio = null
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
